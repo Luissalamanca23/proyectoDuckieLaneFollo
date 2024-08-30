bb541c46e599352617116ec5c89b648f6424dd0b
gabriel <ga.hinostroza@duocuc.cl>
2024-08-30 13:53:43 -0400

    follow-line and apriltag work right

--- packages/apriltag/src/apriltag_node.py
+++ packages/apriltag/src/apriltag_node.py
@@ -2,7 +2,6 @@
 import dt_apriltags
 import cv2
 import tf
-
 import os
 import rospy
 from duckietown.dtros import DTROS, NodeType
@@ -11,112 +10,101 @@ import numpy as np
 from sensor_msgs.msg import CameraInfo
 from duckietown_msgs.msg import LEDPattern
 from sensor_msgs.msg import CompressedImage
-from std_msgs.msg import String
-from std_msgs.msg import ColorRGBA
+from std_msgs.msg import String, Float32, Int32, ColorRGBA, Bool
 
-
-
-class apriltag_node(DTROS):
+class AprilTagNode(DTROS):
 
     def __init__(self, node_name):
-        super(apriltag_node, self).__init__(node_name=node_name, node_type=NodeType.LOCALIZATION)
+        super(AprilTagNode, self).__init__(node_name=node_name, node_type=NodeType.LOCALIZATION)
         self.node_name = node_name
         self.camera_calibration = None
         self.camera_parameters = None
-        self.safeToRunProgram = False
+        self.safe_to_run_program = False
         self._tf_bcaster = tf.TransformBroadcaster()
 
+        # Initialize image variables
         self.grey_img = np.array([])
         self.col_img = None
         self.curr_msg = None
         self.detector = dt_apriltags.Detector()
 
+        # State variables
         self.run = True
-        self.prev_img = None
-        #no detection
         self.curr_col = "WHITE"
+        self.prev_tag = 0
+        self.dist_from_april = float('inf')
+        self.error_from_april = 0
+        self.april_priority = -1
+
+        # Map of sign ID to color
         self.sign_col_map = {
-            #blue = T-intersection
-            153: "BLUE",
-            58: "BLUE", 
-            11: "BLUE", 
-            62: "BLUE", 
-            #red = stop sign
-            24: "RED",
-            26: "RED",
-            #green = UofA Tag
-            57: "GREEN",
-            200: "GREEN",
-            94: "GREEN",
-            93: "GREEN"
+            153: "BLUE", 58: "BLUE", 11: "BLUE", 62: "BLUE",   # T-intersection signs
+            24: "RED", 26: "RED",                              # Stop signs
+            57: "GREEN", 200: "GREEN", 94: "GREEN", 93: "GREEN" # UofA Tags
         }
 
-        self.p = 0
-        self.q = 0
-
-
-
-        # subscribers
+        # Subscribers
         img_topic = f"""/{os.environ['VEHICLE_NAME']}/camera_node/image/compressed"""
         info_topic = f"""/{os.environ['VEHICLE_NAME']}/camera_node/camera_info"""
-        self.img_sub = rospy.Subscriber(img_topic, CompressedImage, self.cb_img, queue_size = 1)
-        self.subscriberCameraInfo = rospy.Subscriber(info_topic, CameraInfo, self.camera_info_callback,  queue_size=1)
+        self.img_sub = rospy.Subscriber(img_topic, CompressedImage, self.cb_img, queue_size=1)
+        self.subscriber_camera_info = rospy.Subscriber(info_topic, CameraInfo, self.camera_info_callback, queue_size=1)
+        self.kill_sub = rospy.Subscriber(f"/{os.environ['VEHICLE_NAME']}/shutdown", Bool, self.cb_kill, queue_size=1)
+        self.april_priority_sub = rospy.Subscriber(f"/{os.environ['VEHICLE_NAME']}/april_priority", Int32, self.cb_april_priority, queue_size=1)
 
-        # publishers
+        # Publishers
         self.pub = rospy.Publisher("/" + os.environ['VEHICLE_NAME'] + '/grey_img/compressed', CompressedImage, queue_size=1)
         self.pub_led = rospy.Publisher("/" + os.environ['VEHICLE_NAME'] + "/led_emitter_node/led_pattern", LEDPattern, queue_size=1)
-        self.pub_april = rospy.Publisher("/april_topic", String, queue_size=1)
-        self.publishLEDs(1.0, 0.0, 0.0)
+        self.dist_from_pub = rospy.Publisher("/" + os.environ['VEHICLE_NAME'] + '/dist_from_april', Float32, queue_size=1)
+        self.april_id = rospy.Publisher("/" + os.environ['VEHICLE_NAME'] + '/april_id', Int32, queue_size=1)
+        self.april_x_error = rospy.Publisher("/" + os.environ['VEHICLE_NAME'] + '/april_x_error', Int32, queue_size=1)
 
-    def sign_to_col(self, id):
-        if(id in self.sign_col_map):
-            return self.sign_col_map[id]
-        else:
-            print("[INFO] Recognized AprilTag - but the ID is not valid.")
-            return "WHITE"
+        # Initialize LEDs to white
+        self.publishLEDs(1.0, 1.0, 1.0)
 
+    def cb_april_priority(self, msg):
+        self.april_priority = msg.data
+
+    def cb_kill(self, msg):
+        self.run = msg.data
 
     def camera_info_callback(self, msg):
         self.camera_calibration = msg
-
         print("== Calibrating Camera ==")
 
-        currRawImage_height = 640
-        currRawImage_width = 480
+        curr_raw_image_height = 640
+        curr_raw_image_width = 480
 
         scale_matrix = np.ones(9)
-        if self.camera_calibration.height != currRawImage_height or self.camera_calibration.width != currRawImage_width:
-            scale_width = float(currRawImage_width) / self.camera_calibration.width
-            scale_height = float(currRawImage_height) / self.camera_calibration.height
+        if self.camera_calibration.height != curr_raw_image_height or self.camera_calibration.width != curr_raw_image_width:
+            scale_width = float(curr_raw_image_width) / self.camera_calibration.width
+            scale_height = float(curr_raw_image_height) / self.camera_calibration.height
             scale_matrix[0] *= scale_width
             scale_matrix[2] *= scale_width
             scale_matrix[4] *= scale_height
             scale_matrix[5] *= scale_height
 
-        self.tag_size = 0.065 #rospy.get_param("~tag_size", 0.065)
+        self.tag_size = 0.065
         rect_K, _ = cv2.getOptimalNewCameraMatrix(
             (np.array(self.camera_calibration.K)*scale_matrix).reshape((3, 3)),
             self.camera_calibration.D,
-            (640,480),
+            (640, 480),
             1.0
         )
         self.camera_parameters = (rect_K[0, 0], rect_K[1, 1], rect_K[0, 2], rect_K[1, 2])
 
-
         try:
-            self.subscriberCameraInfo.shutdown()
-            self.safeToRunProgram = True
+            self.subscriber_camera_info.shutdown()
+            self.safe_to_run_program = True
             print("== Camera Info Subscriber successfully killed ==")
         except BaseException:
             pass
-
 
     def cb_img(self, msg):
         data_arr = np.frombuffer(msg.data, np.uint8)
         col_img = cv2.imdecode(data_arr, cv2.IMREAD_COLOR)
         grey_img = cv2.cvtColor(col_img, cv2.COLOR_BGR2GRAY)
-        self.grey_img = grey_img[1* len(col_img) // 4 : 2 * len(col_img) // 3]
-        self.col_img = col_img[1 * len(col_img) // 4 : 2 * len(col_img) // 3]
+        self.grey_img = grey_img[1 * len(col_img) // 4: 2 * len(col_img) // 3]
+        self.col_img = col_img[1 * len(col_img) // 4: 2 * len(col_img) // 3]
         self.curr_msg = msg
 
     def img_pub(self):
@@ -124,14 +112,11 @@ class apriltag_node(DTROS):
             msg = CompressedImage()
             msg.header.stamp = rospy.Time.now()
             msg.format = "jpeg"
-            msg.data = np.array(cv2.imencode('.jpg', self.grey_img)[1]).tostring()
-
+            msg.data = np.array(cv2.imencode('.jpg', self.grey_img)[1]).tobytes()
             self.pub.publish(msg)
-
 
     def publishLEDs(self, red, green, blue):
         set_led_cmd = LEDPattern()
-
         for i in range(5):
             rgba = ColorRGBA()
             rgba.r = red
@@ -139,117 +124,66 @@ class apriltag_node(DTROS):
             rgba.b = blue
             rgba.a = 1.0
             set_led_cmd.rgb_vals.append(rgba)
-
         self.pub_led.publish(set_led_cmd)
 
-    def april_pub(self):
-        msg = String()
-        msg.data = f"{self.p}:{self.q}"
-        print("april", msg.data)
+    def pub_april_x_error(self):
+        msg = Int32()
+        msg.data = self.error_from_april
+        self.april_x_error.publish(msg)
 
-        self.pub_april.publish(msg)
+    def dist_pub(self):
+        msg = Float32()
+        msg.data = self.dist_from_april * 2  # Adjusted distance
+        self.dist_from_pub.publish(msg)
 
-
-    def change_led_to(self, new_col):
-        # print("col:", new_col)
-
-        if(new_col == "RED"):
-            self.publishLEDs(1.0, 0.0, 0.0)
-
-        elif(new_col == "GREEN"):
-            self.publishLEDs(0.0, 1.0, 0.0)
-
-        elif(new_col == "BLUE"):
-            self.publishLEDs(0.0, 0.0, 1.0)
-
-        else:
-            self.publishLEDs(1.0, 1.0, 1.0)
-
-
-    def convert_pixel_x_to_rad_from_middle(self, pixel_x):
-        # Camera FOV = 160deg => right edge is 80deg from midpoint (and left edge is -80deg)
-        # 80 deg in rad
-        frame_edge_rad = 1.39626
-        camera_res_x = 640 # so here x=640 => +1.39626 rad from centre, x=0 => -1.3626
-
-        percent_offset_from_midpoint = (pixel_x - (camera_res_x/2)) / (camera_res_x/2) # gives value in [-1, 1] with 0 being perfect middle
-
-        rad_from_middle = frame_edge_rad * percent_offset_from_midpoint # gives value in [-1.3626, 1.3626] rad = [-80, 80] deg
-
-        return rad_from_middle
-
-    
     def _matrix_to_quaternion(self, r):
         T = np.array(((0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 1)), dtype=np.float64)
         T[0:3, 0:3] = r
         return tf.transformations.quaternion_from_matrix(T)
 
-
     def detect_tag(self):
-        if(not self.safeToRunProgram):
+        if not self.safe_to_run_program:
             return
 
-
-        # convert the img to greyscale
-        img =  self.col_img
+        img = self.col_img
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
         tags = self.detector.detect(gray, True, self.camera_parameters, self.tag_size)
 
         print("[INFO] {} total AprilTags detected".format(len(tags)))
 
-
         if len(tags) == 0:
             self.change_led_to("WHITE")
             self.curr_col = "WHITE"
-
-            msg = CompressedImage()
-            msg.header.stamp = rospy.Time.now()
-            msg.format = "jpeg"
-            msg.data = np.array(cv2.imencode('.jpg', img)[1]).tostring()
-            self.pub.publish(msg)
+            self.img_pub()
             return
 
-        closest_col = "WHITE" 
+        closest_col = "WHITE"
         closest = 0
 
         for tag in tags:
-            # extract the bounding box (x, y)-coordinates for the AprilTag
-            # and convert each of the (x, y)-coordinate pairs to integers
             (ptA, ptB, ptC, ptD) = tag.corners
             diff = abs(ptA[0] - ptB[0])
-            # print("dif:", diff)
-            ptB = (int(ptB[0]), int(ptB[1]))
-            ptC = (int(ptC[0]), int(ptC[1]))
-            ptD = (int(ptD[0]), int(ptD[1]))
-            ptA = (int(ptA[0]), int(ptA[1]))
+            ptA, ptB, ptC, ptD = map(lambda pt: (int(pt[0]), int(pt[1])), (ptA, ptB, ptC, ptD))
 
-            # draw the bounding box of the AprilTag detection
             line_col = (125, 125, 0)
             cv2.line(img, ptA, ptB, line_col, 2)
             cv2.line(img, ptB, ptC, line_col, 2)
             cv2.line(img, ptC, ptD, line_col, 2)
             cv2.line(img, ptD, ptA, line_col, 2)
 
-            # get the center (x, y)-coordinates of the AprilTag
             (cX, cY) = (int(tag.center[0]), int(tag.center[1]))
 
-            # draw the tag id on the image
             txt_col = (25, 25, 200)
             tag_id = tag.tag_id
             cv2.putText(img, str(tag_id), (cX - 9, cY + 4), cv2.FONT_HERSHEY_SIMPLEX, 0.5, txt_col, 2)
-            print("[INFO] tag id: {}".format(tag_id))
 
-            # if multiple seen, set col to the closest tag
             if diff > closest:
                 closest = diff
                 closest_col = self.sign_to_col(tag_id)
 
-            # turn rotation matrix into quaternion
             self.q = self._matrix_to_quaternion(tag.pose_R)
-            self.p = tag.pose_t.T[0]        
+            self.p = tag.pose_t.T[0]
 
-            # publish tf
             self._tf_bcaster.sendTransform(
                 self.p.tolist(),
                 self.q.tolist(),
@@ -258,25 +192,23 @@ class apriltag_node(DTROS):
                 self.curr_msg.header.frame_id,
             )
 
-        # change the led based on the tag id
         self.change_led_to(closest_col)
         self.curr_col = closest_col
+        self.img_pub()
 
-        # publish the image with the tag id and box to a custom topic
-        msg = CompressedImage()
-        msg.header.stamp = rospy.Time.now()
-        msg.format = "jpeg"
-        msg.data = np.array(cv2.imencode('.jpg', img)[1]).tostring()
-        self.pub.publish(msg)
+    def change_led_to(self, new_col):
+        colors = {"RED": (1.0, 0.0, 0.0), "GREEN": (0.0, 1.0, 0.0), "BLUE": (0.0, 0.0, 1.0), "WHITE": (1.0, 1.0, 1.0)}
+        self.publishLEDs(*colors.get(new_col, (1.0, 1.0, 1.0)))
+
+    def sign_to_col(self, tag_id):
+        return self.sign_col_map.get(tag_id, "WHITE")
 
 if __name__ == '__main__':
-    # create the node
-    node = apriltag_node(node_name='april_tag_detector')
-
-    rate = rospy.Rate(1) # once every 2 s
+    node = AprilTagNode(node_name='april_tag_detector')
+    rate = rospy.Rate(1)
     while not rospy.is_shutdown() and node.run:
         node.change_led_to(node.curr_col)
         node.detect_tag()
-        node.april_pub()
+        node.pub_april_x_error()
+        node.dist_pub()
         rate.sleep()
-    
--- packages/lane_follow/src/lane_follow.py
+++ packages/lane_follow/src/lane_follow.py
@@ -7,148 +7,132 @@ from duckietown_msgs.msg import Twist2DStamped
 from duckietown.dtros import DTROS, NodeType
 import numpy as np
 
-class LaneFollowNode(DTROS):
+class lane_follow_node(DTROS):
+
     def __init__(self, node_name):
-        super(LaneFollowNode, self).__init__(node_name=node_name, node_type=NodeType.LOCALIZATION)
+        super(lane_follow_node, self).__init__(node_name=node_name, node_type=NodeType.LOCALIZATION)
         self.node_name = node_name
 
-        # Initialize parameters
         self.pub_img = None
         self.run = True
 
-        # HSV color values for masking
+        # HSV color values to mask
         self.lower_bound = np.array([20, 45, 25])
         self.upper_bound = np.array([35, 255, 255]) 
-
-        # Driving parameters
+        # Drive speed and ratio of goal vs distance from bot
         self.drive = True
         self.speed = 0.2
         self.omega = 0
-        self.size_ratio = 0.8   # Distance from the center of the Duckiebot to the dotted line
+        self.size_ratio = 0.8   # Distance from center of duckiebot to dotted line
 
-        # PID control parameters
-        self.prev_time = rospy.Time.now().to_sec()
-        self.prev_error = 0.0
+        self.prev_time = rospy.get_time()
+        self.prev_diff = 0.0
         self.integral = 0.0
-
-        # Initial PID values (Kp, Ki, Kd)
-        self.Kp = 1.0
-        self.Ki = 0.0
-        self.Kd = 0.0
-        self.prev_diff = None
-
-        # Ziegler-Nichols parameters (tuning)
-        self.ultimate_gain = 1.0  # Ku
-        self.ultimate_period = 2.0  # Pu
+        # PID parameters: [Kp, Ki, Kd]
+        self.PID = [1.0, 0.1, 0.05]  # Ajusta estos valores según sea necesario
 
         # Subscribers
-        img_topic = f"""/{os.environ['VEHICLE_NAME']}/camera_node/image/compressed"""
+        img_topic = f"/{os.environ['VEHICLE_NAME']}/camera_node/image/compressed"
         self.img_sub = rospy.Subscriber(img_topic, CompressedImage, self.cb_img, queue_size=1)
 
         # Publishers
         self.img_publisher = rospy.Publisher('/masked_image/compressed', CompressedImage, queue_size=1)
+
         twist_topic = f"/{os.environ['VEHICLE_NAME']}/car_cmd_switch_node/cmd"
         self.twist_publisher = rospy.Publisher(twist_topic, Twist2DStamped, queue_size=1)
 
     def cb_img(self, msg):
-        """Callback for processing the incoming image from the Duckiebot camera."""
-        # Convert the image from the camera and apply HSV mask
+        # Get the image from camera and mask over the HSV range set in init
         data_arr = np.frombuffer(msg.data, np.uint8)
         col_img = cv2.imdecode(data_arr, cv2.IMREAD_COLOR)
-        crop_img = col_img[len(col_img) // 3:]  # Crop the image for processing efficiency
-        hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
-        imagemask = cv2.inRange(hsv, self.lower_bound, self.upper_bound)
+        crop = [len(col_img) // 3, -1]
+        hsv = cv2.cvtColor(col_img, cv2.COLOR_BGR2HSV)
+        imagemask = np.asarray(cv2.inRange(hsv[crop[0] : crop[1]], self.lower_bound, self.upper_bound))
 
-        # Find all yellow lane markings
+        # Find all the yellow dotted lines
         contours, _ = cv2.findContours(imagemask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        if contours:
-            # Get the largest yellow stripe
+        # Get the largest yellow stripe
+        largest = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(largest)
+        conts = [largest]
+
+        # Ignore the largest stripe if it is too close to the bot
+        if y > 200:
             largest = max(contours, key=cv2.contourArea)
+            conts.append(largest)
             x, y, w, h = cv2.boundingRect(largest)
+        
+        # Draw visualization stuff
+        image = cv2.drawContours(col_img[crop[0] : crop[1]], conts, -1, (0, 255, 0), 3)
+        image = cv2.line(image, (x, y + h // 2), (x + int((self.size_ratio * (y + h))), y + h), (0, 255, 0), 2)
 
-            # Ignore the largest stripe if it is too close to the bot
-            if y > 200:
-                contours.remove(largest)
-                if contours:
-                    largest = max(contours, key=cv2.contourArea)
-                    x, y, w, h = cv2.boundingRect(largest)
+        imx, imy, _ = image.shape
+        r = imy - y
+        image = cv2.circle(image, (int((len(image[0]) // 2) - r * np.sin(self.omega)), int(imy - r * np.cos(self.omega))), 4, (0, 0, 255), -1)
 
-            # Draw visualization on the image
-            image = cv2.drawContours(crop_img, [largest], -1, (0, 255, 0), 3)
-            image = cv2.line(image, (x, y + h // 2), (x + int((self.size_ratio * (y + h))), y + h), (0, 255, 0), 2)
+        for i in range(len(image)):
+            image[i][len(image[i]) // 2] = [255, 0, 0]
 
-            imx, imy, _ = image.shape
-            r = imy - y
-            image = cv2.circle(image, (int(imx // 2 - r * np.sin(self.omega)), int(imy - r * np.cos(self.omega))), 4, (0, 0, 255), -1)
+        self.pub_img = image
 
-            # Draw center line for reference
-            for i in range(len(image)):
-                image[i][len(image[i]) // 2] = [255, 0, 0]
-
-            self.pub_img = image
-
-            # Move the bot if drive is enabled
-            if self.drive:
-                self.pid(x, y + h // 2, imx // 2)
-
+        # Only move the bot if drive is true
+        if self.drive:
+            # Set this to y - h//2 for English driver mode
+            # Set this to y + h//2 for American driver mode
+            self.pid(x, y + h // 2, len(image[i]) // 2) 
+        
     def img_pub(self):
-        """Publish the processed image."""
         if self.pub_img is not None:
             msg = CompressedImage()
             msg.header.stamp = rospy.Time.now()
             msg.format = "jpeg"
             msg.data = np.array(cv2.imencode('.jpg', self.pub_img)[1]).tobytes()
+
             self.img_publisher.publish(msg)
 
+    # Control the speed and angle of the bot
     def twist_pub(self):
-        """Publish the twist command to control the Duckiebot's movement."""
         if self.drive:
             msg = Twist2DStamped()
             msg.v = self.speed
             msg.omega = self.omega
+
             self.twist_publisher.publish(msg)
 
     def pid(self, x, y, goal):
-        """PID control to calculate the steering angle (omega)."""
+        # Calculate errors
         scale_for_pixel_area = 0.02
-        error = ((x + int((self.size_ratio * y))) - goal) * scale_for_pixel_area
+        diff = ((x + int((self.size_ratio * y))) - goal) * scale_for_pixel_area  
         
-        # Calculate the time difference
-        current_time = rospy.Time.now().to_sec()
-        dt = current_time - self.prev_time if self.prev_time else 1e-3  # Avoid division by zero
+        # Time calculations
+        current_time = rospy.get_time()
+        delta_time = current_time - self.prev_time
         self.prev_time = current_time
 
-        # Update integral and derivative
-        self.integral += error * dt
-        derivative = (error - self.prev_error) / dt
-        self.prev_error = error
+        # Proportional term
+        P_term = self.PID[0] * diff
 
-        # Ziegler-Nichols tuning (adjusting Kp, Ki, Kd)
-        self.Kp = 0.6 * self.ultimate_gain
-        self.Ki = 1.2 * self.ultimate_gain / self.ultimate_period
-        self.Kd = 3 * self.ultimate_gain * self.ultimate_period / 40
+        # Integral term
+        self.integral += diff * delta_time
+        I_term = self.PID[1] * self.integral
 
-        # Calculate the new omega value
-        self.omega = -(self.Kp * error + self.Ki * self.integral + self.Kd * derivative)
-        rospy.loginfo(f"PID error: {error}, Omega: {self.omega}, Kp: {self.Kp}, Ki: {self.Ki}, Kd: {self.Kd}")
+        # Derivative term
+        D_term = self.PID[2] * (diff - self.prev_diff) / delta_time if delta_time > 0 else 0.0
 
-    def shutdown(self):
-        """Custom shutdown procedure to stop the Duckiebot."""
-        rospy.loginfo("Shutting down lane follow node...")
-        self.drive = False
-        self.twist_pub()  # Ensure the robot stops
-        rospy.loginfo("Lane follow node stopped.")
+        # Update omega with the PID output
+        self.omega = -(P_term + I_term + D_term)
 
+        # Store current diff for next iteration
+        self.prev_diff = diff
+        
+        print(f"PID Output - P: {P_term}, I: {I_term}, D: {D_term}, Omega: {self.omega}")
 
 if __name__ == '__main__':
-    # Initialize the node
-    node = LaneFollowNode(node_name='lane_follow_node')
+    # Create the node
+    node = lane_follow_node(node_name='custom_lane_follow')
 
-    # Setup a shutdown hook to stop the robot safely
-    rospy.on_shutdown(node.shutdown)
-
-    rate = rospy.Rate(10)  # 10Hz
+    rate = rospy.Rate(100) # 100 Hz
     while not rospy.is_shutdown() and node.run:
         node.img_pub()
         node.twist_pub()
